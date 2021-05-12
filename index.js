87bc9e41d944312c61aa8fb4e7e32bb120b5e555
truongchauhien <truongchauhien@gmail.com>
2021-05-12 10:12:39 +0700

    Update protos.

--- index.js
+++ index.js
@@ -10,13 +10,22 @@ const protoFiles = {
         authentication: {
             'authentication.proto': path.resolve(__dirname, 'services', 'authentication', 'authentication.proto'),
             'authorization.proto': path.resolve(__dirname, 'services', 'authentication', 'authorization.proto')
+        },
+        tShockGateway: {
+            'tshock_instance_management.proto': path.resolve(__dirname, 'services', 'tshock_gateway', 'tshock_instance_management.proto'),
+            'tshock_player_management.proto': path.resolve(__dirname, 'services', 'tshock_gateway', 'tshock_player_management.proto'),
+            'tshock_user_management.proto': path.resolve(__dirname, 'services', 'tshock_gateway', 'tshock_user_management.proto'),
+            'tshock_group_management.proto': path.resolve(__dirname, 'services', 'tshock_gateway', 'tshock_group_management.proto')
         }
     },
-    tShockPlugins: {
+    tShockPlugins: { // These service is provided in-directly via tShockGateway service. Do not use these services directly.
         tShockManagement: {
             'tshock_user_management.proto': path.resolve(__dirname, 'tshock_plugins', 'tshock_management', 'tshock_user_management.proto'),
             'tshock_group_management.proto': path.resolve(__dirname, 'tshock_plugins', 'tshock_management', 'tshock_group_management.proto'),
             'tshock_player_management.proto': path.resolve(__dirname, 'tshock_plugins', 'tshock_management', 'tshock_player_management.proto')
+        },
+        inGameEconomy: {
+            'banking_service.proto': path.resolve(__dirname, 'tshock_plugins', 'ingame_enconomy', 'banking_service.proto')
         }
     }
 }
